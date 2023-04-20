591a847c3df1d8aa13c8bc7d2be967c57f4f6216
EffectMagical <120435264+EffectMagical@users.noreply.github.com>
2023-04-20 17:02:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -38,14 +38,14 @@ def encryption():
     return render_template('encrypt.html', title='Шифрование', form=form)
 
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+# @app.errorhandler(404)
+# def not_found(error):
+#     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app.errorhandler(400)
-def bad_request(_):
-    return make_response(jsonify({'error': 'Bad Request'}), 400)
+# @app.errorhandler(400)
+# def bad_request(_):
+#     return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
 if __name__ == '__main__':
