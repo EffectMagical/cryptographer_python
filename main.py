f842efacd35d847adba0e294636fa2c15850f9b9
EffectMagical <120435264+EffectMagical@users.noreply.github.com>
2023-04-23 13:24:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,77 @@
-from flask import Flask, render_template, redirect, make_response, jsonify
+from flask import Flask, render_template, request
 
 from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
-from wtforms.validators import DataRequired, InputRequired
-
-from flask_restful import reqparse, abort, Api, Resource
+from wtforms import TextAreaField, SubmitField, validators, SelectField
 
 from cryptogrs_decrs import *
 
 
 class EncryptionForm(FlaskForm):
-    text_input = TextAreaField("Поле для ввода", validators=[InputRequired()], render_kw={'rows': 20, 'cols': 100})
-    caesar = SubmitField('Цезарь')
-    submit2 = SubmitField('Шифр2')
-    submit3 = SubmitField('Транскрипция1')
-    submit4 = SubmitField('Транскрипция2')
+    ciphers_list = SelectField('Шифры', choices=[(0, 'Цезарь'), (1, 'Шифр'), (2, 'Шифр2'),
+                                                 (3, 'Транскрипция1'), (4, 'Транскрипция2'), (5, 'Геошифр')])
+    text_input = TextAreaField("Введите текст (макс символов: 1000)", [validators.InputRequired(),
+                                                                       validators.Length(max=1000,
+                                                                                         message='Макс. символов: 1000')],
+                               render_kw={'rows': 15, 'cols': 60})
+    cipher_btn = SubmitField('Зашифровать')
+
+
+class DecryptionForm(FlaskForm):
+    ciphers_list2 = SelectField('Шифры', choices=[(0, 'Цезарь'), (1, 'Шифр'),
+                                                  (3, 'Транскрипция1'), (4, 'Транскрипция2'), (5, 'Геошифр')],
+                                default=0)
+    text_input2 = TextAreaField("Введите текст (макс символов: 1000)", [validators.InputRequired(),
+                                                                        validators.Length(max=1000,
+                                                                                          message='Макс. символов: 1000')],
+                                render_kw={'rows': 15, 'cols': 60})
+    decipher_btn = SubmitField('Расшифровать')
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/')
-@app.route('/encryption', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def encryption():
-    form = EncryptionForm()
-    if form.validate_on_submit():
-        if form.caesar.data:
-            pass
-            # form.text_input.
-        elif form.submit2.data:
-            pass
-        elif form.submit3.data:
-            pass
-        elif form.submit4.data:
-            pass
-    return render_template('encrypt.html', title='Шифрование', form=form)
+    en_form = EncryptionForm()
+    if en_form.validate_on_submit():
+        if request.method == 'POST':
+            if en_form.ciphers_list.data == '1':
+                en_form.text_input.data = encrypt(en_form.text_input.data)
+            elif en_form.ciphers_list.data == '2':
+                # en_form.text_input.data = encode(en_form.text_input.data) - key?
+                pass
+            elif en_form.ciphers_list.data == '3':
+                en_form.text_input.data = transcription1(en_form.text_input.data)
+            elif en_form.ciphers_list.data == '4':
+                en_form.text_input.data = transcription2(en_form.text_input.data)
+            elif en_form.ciphers_list.data == '5':
+                # en_form.text_input.data = geoshifr(en_form.text_input.data) - city?
+                pass
+            else:  # Цезарь
+                # en_form.text_input.data = caesar_cipher(en_form.text_input.data) - shift?
+                pass
+    return render_template('encrypt.html', title='Шифрование', form=en_form)
 
 
-# @app.errorhandler(404)
-# def not_found(error):
-#     return make_response(jsonify({'error': 'Not found'}), 404)
-
-
-# @app.errorhandler(400)
-# def bad_request(_):
-#     return make_response(jsonify({'error': 'Bad Request'}), 400)
+@app.route('/decryption', methods=['GET', 'POST'])
+def decryption():
+    de_form = DecryptionForm()
+    if de_form.validate_on_submit():
+        if request.method == 'POST':
+            if de_form.ciphers_list2.data == '1':
+                de_form.text_input2.data = decrypt(de_form.text_input2.data)
+            elif de_form.ciphers_list2.data == '3':
+                de_form.text_input2.data = detranscription1(de_form.text_input2.data)
+            elif de_form.ciphers_list2.data == '4':
+                de_form.text_input2.data = detranscription2(de_form.text_input2.data)
+            elif de_form.ciphers_list2.data == '5':
+                # de_form.text_input2.data = degeoshifr(de_form.text_input2.data) - city?
+                pass
+            else:  # Цезарь
+                # de_form.text_input2.data = caesar_decipher(de_form.text_input2.data) - shift?
+                pass
+    return render_template('decrypt.html', title='Расшифрование', form=de_form)
 
 
 if __name__ == '__main__':
